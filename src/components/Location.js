24947fd705b3b3ca4b45b8d97a7b88eafc43a562
thompsian <thompsian@gmail.com>
2022-01-04 12:28:09 -0800

    get all locations from api

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,21 +1,39 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Location() {
-    const [location, setLocation] = useState("")
+    const [newLocation, setNewLocation] = useState("")
+    const [location, setLocation] = useState([])
     
+    useEffect(() => {
+        getAllLocations()
+    },[])
+
     function handleChange(e){
         const {value} = e.target
-        setLocation(value)
+        setNewLocation(value)
+    }
+
+    function getAllLocations(){
+        fetch("http://127.0.0.1:3000/locations")
+        .then(res => res.json()
+        .then(data => setLocation(data)))
     }
 
     return (
         <div>
             <h2>Shopping Locations</h2>
+            <ul>
+                {location.map(item => (
+                    <li key = {item.id}>
+                        {item.name}
+                    </li>
+                ))}
+            </ul>
             <input
                 type="text"
                 placeholder="Add Location"
                 onChange={handleChange}
-                value={location}
+                value={newLocation}
             />
             <button>Submit</button>
         </div>
