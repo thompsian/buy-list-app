f0f66362cf4ab6d60ed50f70e2ead9ddf095dd2a
thompsian <thompsian@gmail.com>
2022-01-26 15:57:47 -0800

    map across items

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -2,27 +2,43 @@ import React, {useState, useEffect} from 'react'
 import { Link, useParams } from 'react-router-dom'
 
 function ItemList() {
+    const [newItem, setNewItem] = useState({
+        name:"",
+        category:"",
+        location_id:1
+    })
+
     const [item, setItem] = useState({
         name:"",
         category:"",
         location_id:1
     })
 
+    useEffect(() => {
+        getLocationItems()
+    },[])
+
+    function getLocationItems(){
+        fetch("http://127.0.0.1:3000/items")
+        .then(res => res.json()
+        .then(data => setItem(data)))
+        .catch(errors => console.log("Error fetching all Items", errors))
+    }
+
     const handleChange = e => {
-        setItem({
-            ...item,
+        setNewItem({
+            ...newItem,
             [e.target.name]:e.target.value
         })
     }
 
     const handleAddSubmit = (e) => {
         e.preventDefault()
-        const itemObject = {item}
 
         fetch('http://127.0.0.1:3000/items', {
             method: 'POST',
             headers: { "Content-Type": "application/json"},
-            body: JSON.stringify(itemObject)
+            body: JSON.stringify(newItem)
         })
     }
 
@@ -31,10 +47,18 @@ function ItemList() {
             <h3>Shopping List for Location Placeholder</h3>
             <h5>Add an Item to the Shopping List</h5>
             <form onSubmit={handleAddSubmit}>
-                <input type="text" name="name" value={item.name} onChange={handleChange}/>
-                <input type="text" name="category" value={item.category} onChange={handleChange}/>
+                <input type="text" name="name" value={newItem.name} onChange={handleChange}/>
+                <input type="text" name="category" value={newItem.category} onChange={handleChange}/>
                 <button>Add</button>
             </form>
+            <ul>
+                {item.map(item => (
+                    <li key = {item.id}>
+                        {item.name}
+                        {item.category}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
