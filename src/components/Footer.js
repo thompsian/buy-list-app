b5fbabebab56e351311883a09207611ac55fddad
thompsian <thompsian@gmail.com>
2022-03-10 15:51:26 -0800

    Footer Basics

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 function Footer() {
   return (
-        <h2>Footer Placeholder</h2>
+        <div>
+            <h6>Copyright 2022: Developed by Ian Thompson</h6>
+            <nav>
+            <ul>
+                <li>
+                    <a href = "https://thompsian.github.io/" target="_blank" rel="noreferrer">Portfolio</a>
+                </li>
+                <li>
+                    <a href = "https://www.linkedin.com/in/thompsian/" target="_blank" rel="noreferrer">LinkedIn</a>
+                </li>
+                <li>
+                    <a href = "https://github.com/thompsian/buy-list-app/" target="_blank" rel="noreferrer">WebApp Github</a>
+                </li>
+                <li>
+                    <a href = "https://github.com/thompsian/BuyListAPI/" target="_blank" rel="noreferrer">API Github</a>
+                </li>
+            </ul>
+        </nav>
+        </div>
   );
 }
 
