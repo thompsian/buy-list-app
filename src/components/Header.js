4e6b20af22f7b4c252b63f1a7e9141cb128f6d5f
thompsian <thompsian@gmail.com>
2022-05-23 14:25:36 -0700

    Add styled components to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React from 'react'
 import { Link } from "react-router-dom"
+import styled from 'styled-components'
+
+const Title = styled.h1`
+
+`
+const SubTitle = styled.h6`
+
+`
+const NavBar = styled.nav`
+
+`
 
 function Header() {
   return (
     <div>
-        <h1>BuyList</h1>
-        <h6>Open Source Shopping List Progressive Web Application</h6>
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-            </ul>
-        </nav>
+        <Title>BuyList</Title>
+        <SubTitle>Open Source Shopping List Progressive Web Application</SubTitle>
+        <NavBar>
+            <Link to="/">Home</Link>
+        </NavBar>
     </div>
   );
 }
