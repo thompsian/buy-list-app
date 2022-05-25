8ded5048a74d28998866939df438c86808a2129e
thompsian <thompsian@gmail.com>
2022-05-24 18:15:06 -0700

    Add Item styling added

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,6 +1,42 @@
 import React, { useState , useEffect, useContext } from 'react'
 import { useParams } from 'react-router-dom'
 import {ItemContext} from '../contexts/itemContext'
+import styled from 'styled-components'
+
+const Section = styled.div`
+    background-color: #f5f5f0;
+    border-radius: 15px; 
+    margin: 0 auto;
+    padding: 15px;
+`
+const Title = styled.h3`
+    font-size: 22px;
+    margin-top: 10px;
+    text-align: left;
+`
+const SubTitle = styled.h5`
+    margin-top: 10px;
+    text-align: left;
+`
+const StyledForm = styled.form`
+
+`
+const StyledButton = styled.button`
+    padding: 8px 20px;
+    text-align: center;
+    font-size: 16px;
+    margin: 10px;
+    &:hover {
+        font-weight: bold;
+    }
+`
+const StyledLabel = styled.label`
+    padding: 5px;
+
+`
+const StyledInput = styled.input`
+    margin: 5px;
+`
 
 function AddItem(props) {
     const { locationID } = useParams()
@@ -47,15 +83,17 @@ function AddItem(props) {
     }
 
     return (
-        <div>
-            {loading ? <h3>Data is Loading</h3>:<h3>Shopping List for {locationInfo[0].name}</h3>}
-            <h5>Add an Item to the Shopping List</h5>
-            <form onSubmit={handleAddSubmit}>
-                <input type="text" name="name" value={newItem.name} onChange={handleChange}/>
-                <input type="text" name="category" value={newItem.category} onChange={handleChange}/>
-                <button>Add</button>
-            </form>
-        </div>
+        <Section>
+            {loading ? <Title>Data is Loading</Title>:<Title>Shopping List for {locationInfo[0].name}</Title>}
+            <SubTitle>Add an Item to the Shopping List:</SubTitle>
+            <StyledForm onSubmit={handleAddSubmit}>
+                <StyledLabel>Item Name:</StyledLabel>
+                <StyledInput type="text" name="name" value={newItem.name} onChange={handleChange}/>
+                <StyledLabel>Item Category:</StyledLabel>
+                <StyledInput type="text" name="category" value={newItem.category} onChange={handleChange}/>
+                <StyledButton>Add</StyledButton>
+            </StyledForm>
+        </Section>
     );
 }
 
