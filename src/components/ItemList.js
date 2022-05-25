26e1baf709d48e2e851fc7180d289149919f97da
thompsian <thompsian@gmail.com>
2022-05-24 18:52:18 -0700

    Item List styling added

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,6 +1,41 @@
 import React, {useState, useEffect, useContext} from 'react'
 import { useParams } from 'react-router-dom'
 import {ItemContext} from '../contexts/itemContext'
+import styled from 'styled-components'
+
+const Section = styled.div`
+    border-radius: 15px; 
+    margin: 0 auto;
+    padding: 15px;
+`
+
+const ListSection = styled.ul`
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+`
+const ListItem = styled.li`
+    display:flex;
+    border-style: solid;
+    border-width: 1px;
+    border-radius: 15px; 
+    margin: 5px;
+`
+const StyledButton = styled.button`
+    padding: 8px 25px;
+    text-align: center;
+    font-size: 16px;
+    margin: 10px;
+    &:hover {
+        font-weight: bold;
+    }
+    margin-left: auto;
+`
+
+const StyledParagraph = styled.p`
+    flex:2;
+    padding: 5px;
+`
 
 function ItemList() {
     const { locationID } = useParams()
@@ -34,17 +69,17 @@ function ItemList() {
     }
     
     return (
-        <div>
-            <ul>
-                {loading ? <li>Data is Loading</li> : items.map(itemList => ( itemList.location_id == locationID && 
-                    <li key = {itemList.id}>
-                        {itemList.name}
-                        {itemList.category}
-                        <button onClick={(e) => handleDeleteItem(itemList.id, e)}>Item In Cart</button>
-                    </li>
+        <Section>
+            <ListSection>
+                {loading ? <ListItem>Data is Loading</ListItem> : items.map(itemList => ( itemList.location_id == locationID && 
+                    <ListItem key = {itemList.id}>
+                        <StyledParagraph>{itemList.name}</StyledParagraph>
+                        <StyledParagraph>{itemList.category}</StyledParagraph>
+                        <StyledButton onClick={(e) => handleDeleteItem(itemList.id, e)}>Item In Cart</StyledButton>
+                    </ListItem>
                 ))}
-            </ul>
-        </div>
+            </ListSection>
+        </Section>
     );
 }
 
