b7a5b506353d9171f602a27a28b8ce4a7cff343b
thompsian <thompsian@gmail.com>
2022-06-08 10:06:26 -0700

    AddLocation fireEvents

--- src/tests/AddLocation.test.js
+++ src/tests/AddLocation.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import AddLocation from '../components/AddLocation';
-import {LocationContext} from '../contexts/locationContext'
+import {LocationContext} from '../contexts/locationContext';
 
 describe('AddLocation Component', () => {
     test('should contain the text add a new location', () => {
@@ -11,5 +11,36 @@ describe('AddLocation Component', () => {
 
         const titleName = screen.getByText(/add a new location/i);
         expect(titleName).toBeInTheDocument();
+    });
+
+    test('should render input element', async () => {
+        render(<LocationContext.Provider value={0}>
+            <AddLocation />
+        </LocationContext.Provider>);
+
+        const inputElement = screen.getByPlaceholderText(/enter location name/i);
+        expect(inputElement).toBeInTheDocument();
+    });
+
+    test('should be able to type into input', async () => {
+        render(<LocationContext.Provider value={0}>
+            <AddLocation />
+        </LocationContext.Provider>);
+
+        const inputElement = screen.getByPlaceholderText(/enter location name/i);
+        fireEvent.change(inputElement, { target: {value: "Location 1"} });
+        expect(inputElement.value).toBe("Location 1");
+    });
+
+    test('should reset input box to empty after submit', async () => {
+        render(<LocationContext.Provider value={0}>
+            <AddLocation />
+        </LocationContext.Provider>);
+
+        const inputElement = screen.getByPlaceholderText(/enter location name/i);
+        const buttonElement = screen.getByRole("button");
+        fireEvent.change(inputElement, { target: {value: "Location 1"} });
+        fireEvent.click(buttonElement);
+        expect(inputElement.value).toBe("Location 1");
     });
 });
