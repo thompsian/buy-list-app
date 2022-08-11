6139205ac81849f647caa300626bbf7f050f2c6c
thompsian <thompsian@gmail.com>
2022-08-11 14:13:04 -0700

    Prepare App.js containers

--- src/App.js
+++ src/App.js
@@ -5,21 +5,30 @@ import Main from './pages/Main'
 import List from './pages/List'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import {DataContextProvider} from './contexts/dataContext'
+import styled from 'styled-components'
+
+const ContainerOuter = styled.div`
+
+`
+
+const ContainerContent = styled.div`
+
+`
 
 function App() {
     return (
         <Router>
             <DataContextProvider>
-            <div className="App">
+            <ContainerOuter>
                 <Header />
-                <div className="content">
+                <ContainerContent>
                     <Routes>
                         <Route path="/" element={<Main/>} />
                         <Route path="/lists/:locationID" element={<List/>} />
                     </Routes>
-                </div>
+                </ContainerContent>
                 <Footer />
-            </div>
+            </ContainerOuter>
             </DataContextProvider>
         </Router>
     );
