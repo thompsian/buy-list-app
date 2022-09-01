edb34ee390559c74c4ec9bb0be00953ab3b14a0a
thompsian <thompsian@gmail.com>
2022-09-01 10:03:21 -0700

    limit character length of location name

--- src/components/AddLocation.js
+++ src/components/AddLocation.js
@@ -38,20 +38,27 @@ const AddButton = styled.button`
     }
 `
 
-function AddLocation(props) {
+function AddLocation() {
     const [name, setName] = useState("")
     const { addLocationCount, addCount } = useContext(DataContext)
     const addressAPI = process.env.REACT_APP_BASE_API_URL
 
+    const handleChange = (e) => {
+        if (e.target.value.length === 18){
+            window.alert("Location name should not exceed 18 characters")
+        }
+        setName(e.target.value)
+    }
+    
     const handleSubmit = (e) => {
         e.preventDefault()
 
-        const locationObject = {name}
+        const locationName = {name}
 
         fetch(`${addressAPI}/locations`, {
             method: 'POST',
             headers: { "Content-Type": "application/json"},
-            body: JSON.stringify(locationObject)
+            body: JSON.stringify(locationName)
         })
         .then(() => setName(""))
         .then(() => addCount())
@@ -67,7 +74,8 @@ function AddLocation(props) {
                     required
                     placeholder="Enter Location Name"
                     value={name}
-                    onChange={(e) => setName(e.target.value)}
+                    maxLength={18}
+                    onChange={handleChange}
                 />
                 <AddButton aria-label="Submit the Add Location Form">Add</AddButton>
             </AddForm>
