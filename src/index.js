f5fb78d38d2bfc0a94a742c29e99457f40f3f37e
thompsian <thompsian@gmail.com>
2022-09-23 09:06:51 -0700

    Set up themeContext

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import {ThemeContextProvider} from './contexts/themeContext'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeContextProvider>
+      <App />
+    </ThemeContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
