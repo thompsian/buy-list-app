68c446535e6adb116e9845763e69edb84c37d162
thompsian <thompsian@gmail.com>
2023-07-27 14:05:01 -0700

    Location dark mode styling

--- src/components/Location.js
+++ src/components/Location.js
@@ -21,12 +21,34 @@ const TitleSection = styled.div`
 
 const SortSelect = styled.select`
     flex: 1;
-    font-size: 1rem;
+    font-size: 20px;
+    font-weight: bold;
     text-align: center;
     margin-right: 1em;
     padding: .5em 2em;
     cursor: pointer;
+    appearance: none;
+    color: #444;
+    border: 1px solid #aaa;
+	box-shadow: 0 1px 0 1px rgba(0,0,0,.04);
+	border-radius: .5em;
+    background-color: #fff;
+	background-image: url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23007CB2%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E'),
+	  linear-gradient(to bottom, #ffffff 0%,#e5e5e5 100%);
+	background-repeat: no-repeat, repeat;
+	background-position: right .7em top 50%, 0 0;
+	background-size: .65em auto, 100%;
 
+    &:hover {
+        border-color: #888;
+    }
+    &:focus {
+        border-color: #aaa;
+        box-shadow: 0 0 1px 3px rgba(59, 153, 252, .7);
+        box-shadow: 0 0 0 3px -moz-mac-focusring;
+        color: #222;
+        outline: none;
+    }
 
     @media (max-width: 650px) {
         margin-right: 0;
@@ -34,11 +56,11 @@ const SortSelect = styled.select`
 
 const SortButton = styled.button`
     font-size: 1rem;
-    padding: .5em 2em;
+    padding: .7em 2em;
     text-align: center;
     margin-left: auto;
     cursor: pointer;
-    border-radius: 2rem;
+    border-radius: 3rem;
     background: ${props => (props.theme.mode === "dark" ? "#e6f2ff" : "#00264d")};
     color: ${props => (props.theme.mode === "dark" ? "#000000" : "#ffffff")};
     &:hover {
@@ -63,7 +85,7 @@ const ListSection = styled.ul`
 `
 const ListItem = styled.li`
     display: flex;
-    background-color: #f5f5f0;
+    background-color: ${props => (props.theme.mode === "dark" ? "#4d4d33" : "#f5f5f0")};
     border-radius: 15px; 
     margin: 0.5em;
     padding: 1.5em;
@@ -73,6 +95,7 @@ const LocationName = styled.h4`
     font-size: 3rem;
     font-weight: bold;
     white-space: nowrap;
+    color: ${props => (props.theme.mode === "dark" ? "#ffffff" : "#000000")};
 `
 const StyledLink = styled(Link)`  
     color: Black;
@@ -96,11 +119,20 @@ const DeleteButton = styled.button`
     text-align: center;
     font-size: 1rem;
     margin: .5m;
+    border-radius: 3rem;
     &:hover {
        cursor: pointer;
     }
     margin-left: auto;
     white-space: nowrap;
+    background: ${props => (props.theme.mode === "dark" ? "#ffb3b3" : "#4d0000")};
+    color: ${props => (props.theme.mode === "dark" ? "#000000" : "#ffffff")};
+    &:hover {
+        background: ${props => (props.theme.mode === "dark" ? "#ffcccc" : "#800000")};
+    }
+    &:focus {
+        background: ${props => (props.theme.mode === "dark" ? "#ffcccc" : "#800000")};
+    }
 `
 
 const ShowMoreSection = styled.div`
@@ -114,8 +146,15 @@ const LoadMoreButton = styled.button`
     font-size: 1rem;
     margin: .5em;
     width: 80%;
+    border-radius: 3rem;
+    cursor: pointer;
+    background: ${props => (props.theme.mode === "dark" ? "#e6f2ff" : "#00264d")};
+    color: ${props => (props.theme.mode === "dark" ? "#000000" : "#ffffff")};
     &:hover {
-       cursor: pointer;
+        background: ${props => (props.theme.mode === "dark" ? "#99ccff" : "#004d99")};
+    }
+    &:focus {
+        background: ${props => (props.theme.mode === "dark" ? "#99ccff" : "#004d99")};
     }
 `
 const EmptyLoadMoreDiv = styled.div`
